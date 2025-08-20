ac1b7e6c7028d191f4566f284f0f7e9c51f143b0
navnishan <navnishann@gmaol.com>
2025-08-20 21:35:12 +0300

    cinema visit

---
+++ app/cinema/hall.py
@@ -0,0 +1,20 @@
+from typing import Iterable
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
+
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+        self,
+        movie_name: str,
+        customers: Iterable[Customer],
+        cleaning_staff: Cleaner,
+    ) -> None:
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        for customer in customers:
+            customer.watch_movie(movie=movie_name)
+        print(f"\"{movie_name}\" ended.")
+        cleaning_staff.clean_hall(hall_number=self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-# write your imports here
+from typing import List, Dict
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    customers: List[Dict[str, str]],
+    hall_number: int,
+    cleaner: str,
+    movie: str,
+) -> None:
+    customer_objs = [
+        Customer(name=c["name"], food=c["food"])
+        for c in customers
+    ]
+
+    for cust in customer_objs:
+        CinemaBar.sell_product(product=cust.food, customer=cust)
+
+    hall = CinemaHall(number=hall_number)
+    cleaner_obj = Cleaner(name=cleaner)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_objs,
+        cleaning_staff=cleaner_obj,
+    )
